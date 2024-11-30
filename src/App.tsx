801592226f92528cdacc0feb3ca88566b8ffffda
Daniel Olguin <dani.olg@hotmail.com>
2024-11-30 19:13:48 -0300

    feat: add ts import resolver

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 
-import { Home } from './pages';
+import { Home } from '@/pages';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
